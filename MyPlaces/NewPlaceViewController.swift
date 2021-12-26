f23bffe03021f6513a8955eb97c9c12355d3de82
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2021-12-26 16:54:51 +0300

    Add editing function

--- MyPlaces/NewPlaceViewController.swift
+++ MyPlaces/NewPlaceViewController.swift
@@ -64,7 +64,7 @@ class NewPlaceViewController: UITableViewController {
         }
     }
 
-    func saveNewPlace(){
+    func savePlace(){
         
         
         var image: UIImage?
@@ -81,21 +81,53 @@ class NewPlaceViewController: UITableViewController {
                              location: placeLocation.text,
                              type: placeType.text,
                              imageData: imageData)
-        StorageManager.saveObject(newPlace)
+        if currentPlace != nil {
+            
+            try! realm.write {
+                currentPlace?.name = newPlace.name
+                currentPlace?.location = newPlace.location
+                currentPlace?.type = newPlace.type
+                currentPlace?.imageData = newPlace.imageData
+            }
+            
+        } else {
+            
+            StorageManager.saveObject(newPlace)
+            
+        }
+        
+        
     }
     
     private func setupEditScreen() {
+        
+        setupNavigationBar()
+        imageIsChanged = true
         
         if currentPlace != nil {
             
             guard let data = currentPlace?.imageData, let image = UIImage(data: data) else {return}
             
             placeImage.image = image
+            placeImage.contentMode = .scaleAspectFill
             placeName.text = currentPlace?.name
             placeLocation.text = currentPlace?.location
             placeType.text = currentPlace?.type
             
         }
+    }
+    private func setupNavigationBar() {
+        
+        if let topItem = navigationController?.navigationBar.topItem {
+            
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+            
+        }
+        
+        navigationItem.leftBarButtonItem = nil
+        title = currentPlace?.name
+        saveButton.isEnabled = true
+        
     }
     
     @IBAction func canceAction(_ sender: Any) {
