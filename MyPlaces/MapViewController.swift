23105a99a78d9fb3669981fcc575dcb46058c427
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-01-03 12:21:26 +0300

    prepare to add maps

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Артур Фомин on 03.01.2022.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    @IBAction func closVC() {
+        dismiss(animated: true)
+    }
+}
