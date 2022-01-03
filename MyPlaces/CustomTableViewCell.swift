1ebf596238ed3b904b4d26aaa7ebfcf914b3f901
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-01-03 11:26:57 +0300

    optimize code

--- MyPlaces/CustomTableViewCell.swift
+++ MyPlaces/CustomTableViewCell.swift
@@ -7,7 +7,12 @@ import UIKit
 
 class CustomTableViewCell: UITableViewCell {
 
-    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var imageOfPlace: UIImageView! {
+        didSet{
+            imageOfPlace.layer.cornerRadius = imageOfPlace.frame.size.height / 2
+            imageOfPlace.clipsToBounds = true
+        }
+    }
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var locationLabel: UILabel!
     @IBOutlet var typeLabel: UILabel!
